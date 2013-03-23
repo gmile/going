a7d6c47caa8be6c147e8ffaf825fbf1d49162474
Pirogov Evgenij <iamexile@gmail.com>
2013-03-23 18:07:27 +0200

    Extract inventory logic

--- program.go
+++ program.go
@@ -2,33 +2,14 @@ package main
 
 import (
   "fmt"
-  "os"
-  csv "encoding/csv"
-  "car"
+  i "inventory"
 )
 
-func readCsv(path string) []car.Car {
-  file, err := os.Open(path)
-  if err != nil {
-    panic(err)
-  }
-  defer file.Close()
-
-  matrix, err := csv.NewReader(file).ReadAll()
-
-  cars := make( []car.Car, len(matrix)-1 )
-
-  for i, row := range matrix[1:] {
-    cars[i].Initialize(row)
-  }
-
-  return cars
-}
-
 func main() {
-  var cars []car.Car = readCsv("/Users/gmile/odesk/haystack_profiling/homenet.txt")
+  var inventory i.Inventory
+  inventory.Initialize("/Users/gmile/odesk/haystack_profiling/homenet.txt")
 
-  for _, car := range cars[1:5] {
+  for _, car := range inventory.Cars[0:5] {
     fmt.Println(car.Model, "was made in", car.Year)
   }
 }
