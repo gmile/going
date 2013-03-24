e908d34c9cc4a37b854d35a17a28c9cbdb2c72bb
Pirogov Evgenij <iamexile@gmail.com>
2013-03-24 02:22:06 +0200

    Update map examples

--- examples/map.go
+++ examples/map.go
@@ -6,12 +6,23 @@ import (
 )
 
 func main() {
+  fmt.Println("Example 1")
   hashmap := map[string]int{}
   key := ""
 
   for i := 0; i < 4; i++ {
     key = strconv.Itoa(i)
-    hashmap[key] = i+15
+    hashmap[key + " -"] = i+15
     fmt.Println(hashmap[key])
+  }
+
+  fmt.Println("Example 2")
+  not_exists, ok := hashmap["example"]
+  fmt.Println(not_exists, ok)
+
+  fmt.Println("Example 3")
+
+  for k, v := range hashmap {
+    fmt.Println(k, v)
   }
 }
