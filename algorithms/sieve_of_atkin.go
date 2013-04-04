4e4a2b12adfc42e008909f10a3b01d9cef1bddb1
Pirogov Evgenij <iamexile@gmail.com>
2013-04-05 01:32:15 +0300

    Fix sieve of Atkin

--- algorithms/sieve_of_atkin.go
+++ algorithms/sieve_of_atkin.go
@@ -5,48 +5,53 @@ import (
 )
 
 const LIMIT = 10000
-var int n
+
+var (
+  n, q int
+)
 
 func main() {
   prime_bools := [LIMIT + 1]bool{}
 
   for i := 0; i < 5; i++ {
-    prime_bools := true
+    prime_bools[i] = true
   }
 
-  sqrt = m.Sqrt(LIMIT)
+  sqrt := int( m.Ceil(m.Sqrt(LIMIT)) )
 
-  field := make(int[][], sqrt)
+  field := make([][]int, sqrt)
   for i, _ := range field {
-    field[i] = make(int[], sqrt)
+    field[i] = make([]int, sqrt)
   }
+
 
   for i := 1; i <= sqrt; i++ {
-    for j := 1; j <= sqrt; i++ {
-      n = 4 * m.Pow(i, 2) + m.Pow(j, 2)
+    for j := 1; j <= sqrt; j++ {
+      n = int( 4 * m.Pow(float64(i), 2) + m.Pow(float64(j), 2) )
       if (n <= LIMIT) && (n % 12 == 1 || n % 12 == 5) { prime_bools[n] = !prime_bools[n] }
 
-      n = 3 * m.Pow(i, 2) + m.Pow(j, 2)
+      n = int( 3 * m.Pow(float64(i), 2) + m.Pow(float64(j), 2) )
       if (n <= LIMIT) && (n % 12 == 7)                { prime_bools[n] = !prime_bools[n] }
 
-      n = 4 * m.Pow(i, 2) - m.Pow(j, 2)
+      n = int( 3 * m.Pow(float64(i), 2) - m.Pow(float64(j), 2) )
       if (i > j) && (n <= LIMIT) && (n % 12 == 11)    { prime_bools[n] = !prime_bools[n] }
     }
   }
 
   for i := 5; i <= LIMIT; i++ {
     if prime_bools[i] {
-      q = m.Pow(i, 2)
+      q = int( m.Pow(float64(i), 2) )
 
-      k := 1; k <= LIMIT; k++ {
+      for k := 1; k*q <= LIMIT; k++ {
         prime_bools[k*q] = false
       }
     }
   }
 
-  print(2, 3)
+  println(2)
+  println(3)
 
   for i := 5; i <= LIMIT; i++ {
-    if prime_bools { print i }
+    if prime_bools[i] { println(i) }
   }
 }
