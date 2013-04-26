0958303c4e0a0e993fae5008a1ae2721de2b6bed
Pirogov Evgenij <iamexile@gmail.com>
2013-04-27 02:19:56 +0300

    Split to functions

--- examples/tictacgoe/server.go
+++ examples/tictacgoe/server.go
@@ -1,12 +1,32 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net"
+)
 
 func print_matrix(m *[3][3]rune) {
   fmt.Printf("\n    1   2   3\n")
   fmt.Printf(" a [%c] [%c] [%c]\n", m[0][0], m[0][1], m[0][2])
   fmt.Printf(" b [%c] [%c] [%c]\n", m[1][0], m[1][1], m[1][2])
   fmt.Printf(" c [%c] [%c] [%c]\n", m[2][0], m[2][1], m[2][2])
+}
+
+func turn(m *[3][3]rune, mark rune, x, y byte) {
+  (*m)[y][x] = mark
+}
+
+func opponents_turn(m *[3][3]rune, mark rune, row rune, col byte) {
+  y := 0
+  x := col - 1
+
+  switch row {
+  case 'a': y = 0
+  case 'b': y = 1
+  case 'c': y = 2
+  }
+
+  (*m)[y][x] = mark
 }
 
 func main() {
@@ -17,22 +37,9 @@ func main() {
 
   print_matrix(&m)
 
-  received_row  := 'b'
-  received_col  := 1
-  received_mark := 'X'
+  turn(&m, 'X', 1, 2)
 
-  y := received_row
-  x := received_col - 1
-
-  switch y {
-  case 'a': y = 0
-  case 'b': y = 1
-  case 'c': y = 2
-  }
-
-  m[y][x] = received_mark
-  fmt.Printf("\n Waiting for opponent...")
-  fmt.Printf("\n[%d, %d] = %c\n", x, y, received_mark)
+  fmt.Printf("\n Waiting for opponent...\n")
 
   print_matrix(&m)
 }
