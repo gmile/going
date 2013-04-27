36539246c14c09699a8b94e9a893ca6575008c0b
Pirogov Evgenij <iamexile@gmail.com>
2013-04-28 01:16:59 +0300

    Make the server more sophisticated

--- examples/tictacgoe/server.go
+++ examples/tictacgoe/server.go
@@ -6,18 +6,24 @@ import (
 	"net"
 )
 
-func print_matrix(m *[3][3]rune) {
+type Game struct {
+  player1_mark rune
+  player2_mark rune
+  game_starts_from byte
+  conn net.Conn
+  board [3][3]rune
+}
+
+func (game *Game) DrawBoard() {
 	fmt.Printf("\n    1   2   3\n")
-	fmt.Printf(" a [%c] [%c] [%c]\n", m[0][0], m[0][1], m[0][2])
-	fmt.Printf(" b [%c] [%c] [%c]\n", m[1][0], m[1][1], m[1][2])
-	fmt.Printf(" c [%c] [%c] [%c]\n", m[2][0], m[2][1], m[2][2])
+	fmt.Printf(" a [%c] [%c] [%c]\n", game.board[0][0], game.board[0][1], game.board[0][2])
+	fmt.Printf(" b [%c] [%c] [%c]\n", game.board[1][0], game.board[1][1], game.board[1][2])
+	fmt.Printf(" c [%c] [%c] [%c]\n", game.board[2][0], game.board[2][1], game.board[2][2])
 }
 
-func turn(m *[3][3]rune, mark rune, x, y byte) {
-	(*m)[y][x] = mark
-}
+// TODO: Split this into GameCore, GameClient and GameServer
 
-func opponents_turn(m *[3][3]rune, mark rune, row rune, col byte) {
+func (game *Game) RecordTurn(mark rune, row rune, col byte) {
 	y := 0
 	x := col - 1
 
@@ -30,32 +36,129 @@ func opponents_turn(m *[3][3]rune, mark rune, row rune, col byte) {
 		y = 2
 	}
 
-	(*m)[y][x] = mark
+	game.board[y][x] = mark
+
+  game.DrawBoard()
+}
+
+func (game *Game) Player1Turn() {
+  fmt.Printf("\nMake a turn:\n")
+
+  // TODO: ask for input
+  var col byte = 1
+  row := 'a'
+
+  my_turn := bufio.NewWriter(game.conn)
+  defer my_turn.Flush()
+
+  my_turn.WriteRune(row)
+  my_turn.WriteByte(col)
+
+  game.RecordTurn(game.player1_mark, row, col)
+}
+
+func (game *Game) Player2Turn() {
+  fmt.Printf("\nWaiting for opponent...\n")
+
+  his_turn := bufio.NewReader(game.conn)
+
+  row, _, _ := his_turn.ReadRune()
+  col, _    := his_turn.ReadByte()
+
+  game.RecordTurn(game.player2_mark, row, col)
+}
+
+func (game *Game) Start() {
+	server, _ := net.Listen("tcp", ":1234")
+  defer server.Close()
+
+  fmt.Printf("Waiting for other player to connect...")
+
+  conn, _ := server.Accept()
+  defer conn.Close()
+
+  fmt.Printf(" opponent connected!\n")
+
+  game.conn = conn
+
+  game.set_marks()
+  game.set_turn_sequence()
+  game.play()
+}
+
+func (game *Game) Join() {
+  conn, _ := net.Dial("tcp", ":1234")
+  defer conn.Close()
+
+  game.conn = conn
+
+  game.get_marks()
+  game.get_turn_sequence()
+  game.play()
+}
+
+func (game *Game) set_turn_sequence() {
+  // TODO: pick this randomly from (0, 1)
+  game.game_starts_from = 2
+
+  writer := bufio.NewWriter(game.conn)
+  writer.WriteByte(game.game_starts_from)
+  writer.Flush()
+}
+
+func (game *Game) get_turn_sequence() {
+  reader := bufio.NewReader(game.conn)
+  game.game_starts_from, _ = reader.ReadByte()
+}
+
+func (game *Game) set_marks() {
+  // TODO: pick these randomly
+  player1_mark := 'X'
+  player2_mark := 'O'
+
+  game.player1_mark = player1_mark
+  game.player2_mark = player2_mark
+
+  marks := bufio.NewWriter(game.conn)
+  defer marks.Flush()
+
+  marks.WriteRune(player1_mark)
+  marks.WriteRune(player2_mark)
+}
+
+func (game *Game) get_marks() {
+  marks := bufio.NewReader(game.conn)
+
+  game.player1_mark, _, _ = marks.ReadRune()
+  game.player2_mark, _, _ = marks.ReadRune()
+}
+
+func (game *Game) play() {
+  game.board = [3][3]rune{
+    {' ', ' ', ' '},
+    {' ', ' ', ' '},
+    {' ', ' ', ' '}}
+
+  game.DrawBoard()
+
+  if game.game_starts_from == 1 {
+    fmt.Printf("Your turn is first.\n")
+
+    for {
+      game.Player1Turn()
+      game.Player2Turn()
+    }
+  } else {
+    fmt.Printf("Opponent's turn is first.\n")
+
+    for {
+      game.Player2Turn()
+      game.Player1Turn()
+    }
+  }
 }
 
 func main() {
-	m := [3][3]rune{
-		{' ', ' ', ' '},
-		{' ', ' ', ' '},
-		{' ', ' ', ' '}}
-
-	listener, _ := net.Listen("tcp", ":1234")
-
-	fmt.Printf("\n Waiting for opponent...\n")
-
-  // my_mark := 'X'
-  // opponents_mark := 'O'
-
-	for {
-		conn, _ := listener.Accept()
-		defer conn.Close()
-		reader := bufio.NewReader(conn)
-
-		received_rune := 'X'
-		received_row, _, _ := reader.ReadRune()
-		received_col, _ := reader.ReadByte()
-
-		opponents_turn(&m, received_rune, received_row, received_col)
-		print_matrix(&m)
-	}
+  game := new(Game)
+  game.Start() // should be game.Start('127.0.0.1', ':1234')
 }
