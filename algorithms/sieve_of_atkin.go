8630b14f9e2402c61f04788c09e853dc54f27a24
Pirogov Evgenij <iamexile@gmail.com>
2013-06-22 10:18:30 +0300

    Better powering

--- algorithms/sieve_of_atkin.go
+++ algorithms/sieve_of_atkin.go
@@ -27,20 +27,20 @@ func main() {
 
   for i := 1; i <= sqrt; i++ {
     for j := 1; j <= sqrt; j++ {
-      n = int( 4 * m.Pow(float64(i), 2) + m.Pow(float64(j), 2) )
+      n = 4 * i * i + j * j
       if (n <= LIMIT) && (n % 12 == 1 || n % 12 == 5) { prime_bools[n] = !prime_bools[n] }
 
-      n = int( 3 * m.Pow(float64(i), 2) + m.Pow(float64(j), 2) )
+      n = 3 * i * i + j * j
       if (n <= LIMIT) && (n % 12 == 7)                { prime_bools[n] = !prime_bools[n] }
 
-      n = int( 3 * m.Pow(float64(i), 2) - m.Pow(float64(j), 2) )
+      n = 3 * i * i - j * j
       if (i > j) && (n <= LIMIT) && (n % 12 == 11)    { prime_bools[n] = !prime_bools[n] }
     }
   }
 
   for i := 5; i <= LIMIT; i++ {
     if prime_bools[i] {
-      q = int( m.Pow(float64(i), 2) )
+      q = i * i
 
       for k := 1; k*q <= LIMIT; k++ {
         prime_bools[k*q] = false
